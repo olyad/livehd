55595617b07afee5f899fb67e0ae8f01dba603df
olyad <okebede@ucsc.edu>
2021-04-11 23:08:15 -0700

    stuff

--- mmap_lib/include/mmap_str.cpp
+++ mmap_lib/include/mmap_str.cpp
@@ -11,49 +11,5 @@
 //helper functions
 
 find_small_size(const str &my, const str &v, std::size_t pos = 0){
-	char first = ((v.ptr_or_start >> (8 * (v._size-1))) & 0xFF);//different ways
-      size_t retval = 0;
-      bool found_flag = false;
-      int i,j,k;
-      int e_pos_self =0;
-      int e_pos_thier =0;
-      for ( i =0; i <4 ; i++){
-        retval = 0;
-        found_flag = false;
-        e_pos_self =0;
-        e_pos_thier =0;
-        if ((first == ((my.ptr_or_start >> (8 * (3 - i))) & 0xFF)) and  ( pos >= i)) {
-          retval = i;
-          found_flag = true;
-          for ( j = i,  k =1; j< 4; j++,k++){
-            
-            if (((v.ptr_or_start >> (8 * (3 - k))) & 0xFF) != ((my.ptr_or_start >> (8 * (3 - j))) & 0xFF)){
-              found_flag = false;
-              break;
-            }
-          }
-          while(k < v._size){
-            if (k < 4){
-              if(((v.ptr_or_start >> (8 * (3 - k))) & 0xFF)  != my.e[e_pos_self]) {
-
-                found_flag = false;
-                break;
-              }
-            } else {
-              if (v.e[e_pos_thier ] != my.e[e_pos_self]){
-                found_flag = false;
-                e_pos_thier++;
-                break;
-              }
-            }
-            e_pos_self++;
-            k++;
-          }
-
-        }
-        if (found_flag == true) return retval;
-      }
-      //if you havent found the string at this point and this string is < 4 chaars then find returns -1
-      //if((_size < 4 ) and (found_flag == false)) return -1;
-      return -1;
+	
 }
